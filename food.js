0fbfb802675f99f66ad66bce44d9803ba12e9247
RanjanaGD <69150679+RanjanaGD@users.noreply.github.com>
2020-10-15 23:16:29 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,42 @@
+class Food{
+    constructor(){
+        var foodStock 
+        var  lastFed
+         this.image=loadImage("Milk.png")
+    }
+    
+
+
+getFoodStock(){}
+updateFoodStock(){}
+ deductFood(){}
+
+display(){
+var x=80,y=100;
+
+imageMode(CENTER)
+image(this.image,220,220,70,70);
+image(this.image,120,220,70,70);
+image(this.image,320,220,70,70);
+
+
+if(this.foodStock!=0){
+    for(var i=0;i<this.foodStock;i++){
+        if(i%10==0){
+            x=80;
+            y=y+50
+        }
+       image(this.image,x,y,50,50)
+       x=x+30
+    }
+}
+
+
+}
+
+
+
+
+}
+
+
